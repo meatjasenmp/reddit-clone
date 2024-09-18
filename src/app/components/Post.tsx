892cdf5b43e105d85d96538e67cd1a86f08cdaf0
Jasen Peterson <me@jasenmp.com>
2024-09-18 08:22:53 -0500

    Basic styles for posts.

--- src/app/components/Post.tsx
+++ src/app/components/Post.tsx
@@ -4,10 +4,10 @@ export default function Post({ post }: { post: RedditPost }) {
   return (
     <div className="divide-y divide-gray-200 overflow-hidden rounded-lg bg-white shadow text-black mb-4 last:mb-0">
       <div className="px-4 py-5 sm:px-6">
-        <h4>{post.title}</h4>
+        <h4 className="text-lg font-bold">{post.title}</h4>
       </div>
       <div className="px-4 py-5 sm:p-6">
-        <p>Content</p>
+        <p>Image</p>
       </div>
       <div className="px-4 py-4 sm:px-6">
         <p>Footer</p>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import { Roboto } from "next/font/google";
 import "./globals.css";
 
 const roboto = Roboto({
-  weight: "300",
+  weight: ["300", "400", "700"],
   subsets: ["latin"],
 });
 
