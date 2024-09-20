281cdb88d60ce5da60136e1d06b4127f845366f8
Jasen Peterson <me@jasenmp.com>
2024-09-20 16:26:56 -0500

    Clean up.

--- src/app/components/Post.tsx
+++ src/app/components/Post.tsx
@@ -1,14 +1,12 @@
-import ImageComponent from "@/app/components/Image";
 import { RedditPost } from "@/app/api/types";
 
 export default function Post({ post }: { post: RedditPost }) {
-  console.log("post", post);
   return (
     <div className="divide-y divide-gray-200 overflow-hidden rounded-lg bg-white shadow text-black mb-4 last:mb-0">
       <div className="px-4 py-5 sm:px-6">
         <h4 className="text-lg font-bold">{post.title}</h4>
       </div>
-      <div className="px-4 py-5 sm:p-6">{/*<ImageComponent src={post.thumbnail} alt={post.title} />*/}</div>
+      <div className="px-4 py-5 sm:p-6">Image</div>
       <div className="px-4 py-4 sm:px-6">
         <p>Footer</p>
       </div>
