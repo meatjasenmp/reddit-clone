812ace8ca3004f636c072b0951ecc48d99dea7a8
Jasen Peterson <me@jasenmp.com>
2024-09-20 16:27:43 -0500

    Clean up.

--- src/app/components/Post.tsx
+++ src/app/components/Post.tsx
@@ -2,7 +2,7 @@ import { RedditPost } from "@/app/api/types";
 
 export default function Post({ post }: { post: RedditPost }) {
   return (
-    <div className="divide-y divide-gray-200 overflow-hidden rounded-lg bg-white shadow text-black mb-4 last:mb-0">
+    <article className="divide-y divide-gray-200 overflow-hidden rounded-lg bg-white shadow text-black mb-4 last:mb-0">
       <div className="px-4 py-5 sm:px-6">
         <h4 className="text-lg font-bold">{post.title}</h4>
       </div>
@@ -10,6 +10,6 @@ export default function Post({ post }: { post: RedditPost }) {
       <div className="px-4 py-4 sm:px-6">
         <p>Footer</p>
       </div>
-    </div>
+    </article>
   );
 }
