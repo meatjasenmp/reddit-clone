218c3474fec09086ff058c6dafa59b1d7d64cee0
Jasen Peterson <me@jasenmp.com>
2024-09-23 17:33:07 -0500

    Image component. WIP.

--- src/app/components/Image.tsx
+++ src/app/components/Image.tsx
@@ -4,13 +4,7 @@ import { PostProps } from "@/app/components/Post";
 import { handleImagePreview } from "@/app/api/media";
 import Image from "next/image";
 
-interface Image {
-  url: string;
-  width: number;
-  height: number;
-}
-
 export default function ImageComponent({ post }: PostProps) {
-  const image: Image = handleImagePreview(post);
+  const image = handleImagePreview(post);
   return <Image src={image.url} width={image.width} height={image.height} alt={post.title} />;
 }
